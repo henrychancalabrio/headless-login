a3d7faa77a1bc17bd9f950a5fe557ac092cfeed6
Henry Chan <henry.chan@calabrio.com>
2020-02-19 12:39:52 -0800

    removed some tests

--- src/main/java/headless/login/HeadlessLogin.java
+++ src/main/java/headless/login/HeadlessLogin.java
@@ -7,27 +7,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
-
+/**
+ * 
+ * @author henry.chan
+ * Please download phantomJS
+ * saves JSESSIONID to /tmp/jsessionid
+ */
 public class HeadlessLogin {
-
+	public static String phantomJSLocation = "/Users/henry.chan/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs";
+	public static String initialUrl = "http://localhost:8080";
+	
 	public static void main(String args[]) throws Exception {
-		File file = new File("/Users/henry.chan/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs");
+		File file = new File(phantomJSLocation);
         System.setProperty("phantomjs.binary.path", file.getAbsolutePath());
-		//WebDriver driver = new HtmlUnitDriver();
 		WebDriver driver = new PhantomJSDriver();
-		//driver.setJavascriptEnabled(true);
-		
-		// HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME);  
-		  //  driver.setJavascriptEnabled(true);
-		driver.get("http://localhost:8080");
-		
-		//System.out.println("Title of the page is" + driver.getTitle());
-
+		driver.get(initialUrl);
 		WebElement username = driver.findElement(By.id("username"));
 		username.sendKeys("admin@indicee.com");
 		WebElement password = driver.findElement(By.id("password"));
 		password.sendKeys("admin0");
-
 		WebElement login = driver.findElement(By.id("kc-login"));
 		login.click();
 		String jSessionid = driver.manage().getCookieNamed("JSESSIONID").getValue();
